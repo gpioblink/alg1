49c72e7e50bc9d244c8e9909b1874e5c1a73219b
Gpioblink <gpioblink@aol.com>
2019-02-01 03:20:54 +0900

    Add mising B (le10)

---
+++ le10/B.py
@@ -0,0 +1,37 @@
+n = int(input())
+M = []
+dp = [[None for i in range(100)] for i in range(100)]
+
+def calcMatrix(i,j):
+    #print(i,j)
+    if i == j:
+        dp[i][j] = 0
+    elif i == j-1:
+        dp[i][j] = M[i]*M[i+1]*M[i+2]
+    else:
+        for k in range(j-i):
+            #print(i,j,k)
+            #print(dp[i][i+k])
+            #print(dp[i+k+1][j])
+            #print(M[i]*M[k+1]*M[j])
+            current = dp[i][i+k] + dp[i+k+1][j] + M[i]*M[i+k+1]*M[j+1]
+            if k==0:
+                dp[i][j] = current
+            if current < dp[i][j]:
+                dp[i][j] = current 
+
+for i in range(n-1):
+    Q = list(map(int,input().split()))
+    M.append(Q[0])
+Q = list(map(int,input().split()))
+M.append(Q[0])
+M.append(Q[1])
+#print(M)
+
+for i in range(n):
+    for j in range(n-i):
+        calcMatrix(j,i+j)
+        #for k in range(10):
+        # print(dp[k])
+        # print()
+print(dp[0][n-1]) 
